dc2946b3ecaee67861c7274c39304c3a5c3b4fa8
Amee <29559290+zl158218@users.noreply.github.com>
2019-01-30 15:29:03 +0800

    Update ip_agent.py

--- ip_agent.py
+++ ip_agent.py
@@ -8,7 +8,6 @@ def get_ip():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
     }
     for i in range(1, 10):
-        page = random.randint(1, 10)  # 随机页数，自行调整
         url = 'https://www.xicidaili.com/nn/' + str(i)  # 国内高匿代理IP
         html = requests.get(url, headers=headers).text
         pattern = re.compile('<td>([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*)</td>.*?<td>(\d*?)</td>.*?<td>([A-Z]*)</td>', re.S)
