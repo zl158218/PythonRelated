4360013f5dcb16b2c095f5d5624f3fed8c2d160e
走风 <zl158218@163.com>
2022-08-02 22:13:20 +0800

    选择排序

--- knowledge/01.py
+++ knowledge/01.py
@@ -3,43 +3,6 @@ import random
 list_a = [random.randint(1, 50) for x in range(0, 10)]
 print(list_a)
 
-
-# def find_smaill_all(arr):
-#     semlllest = arr[0]
-#     semlllest_index = 0
-#     for i in range(1, len(arr)):
-#         if arr[i] < semlllest:
-#             semlllest = arr[i]
-#             semlllest_index = i
-#     return semlllest_index
-
-# def selection_sort(arr):
-#     new_arr = []
-#     for i in range(len(arr)):
-#         semallet = find_smaill_all(arr)
-#         new_arr.append(arr.pop(semallet))
-#     return new_arr
-
-
-# if __name__ == "__main__":
-#     print(selection_sort(list_a))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def selection_sort(arr):
     """
     选择排序
@@ -54,7 +17,9 @@ def selection_sort(arr):
                 semill_val = arr[i]
 
         new_arr.append(arr.pop(semill_ind))
-    print(new_arr)
+    return new_arr
 
 
-selection_sort(list_a)
+if __name__ == '__main__':
+    new = selection_sort(list_a)
+    print(new)
